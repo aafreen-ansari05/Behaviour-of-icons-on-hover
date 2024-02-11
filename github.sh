ef0e70f704766a3e0444677f25113be9febeaa10
aafreen-ansari05 <aafreenjaved10@gmail.com>
2024-02-11 19:21:08 +0530

    Commit: Sunday 11 February 2024 07:21:08 PM IST

--- github.sh
+++ github.sh
@@ -5,4 +5,5 @@ git add .
 # read update_comment
 # git commit -a -m "updated"
 git commit -a -m "Commit: `date`"
+git remote add origin git@github.com:aafreen-ansari05/Behaviour-of-icons-on-hover.git
 git push
