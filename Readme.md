91e99ac87d577374ca7e6c352ce9fb513bc67613
aafreen-ansari05 <aafreenjaved10@gmail.com>
2024-02-12 12:14:51 +0530

    first commit

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 ## Behaviour of buttons on hover
 
-[text](video.webm)
+[video.webm](https://github.com/aafreen-ansari05/Behaviour-of-icons-on-hover/assets/152703156/221262d5-c5b8-4400-aa82-b6845fde7638)
--- github.sh
+++ github.sh
@@ -1,9 +1,6 @@
-git branch
-git status
+git init
 git add .
-# echo "reason to commit?  "
-# read update_comment
-# git commit -a -m "updated"
-git commit -a -m "Commit: `date`"
+git commit -m "first commit"
+git branch -M main
 git remote add origin git@github.com:aafreen-ansari05/Behaviour-of-icons-on-hover.git
-git push
+git push -u origin main
